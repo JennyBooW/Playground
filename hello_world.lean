331340294a8f53635c722591aea086fce4fb0bbb
JennyBooW <verikgorohova@yandex.com>
2024-09-27 19:15:06 +0300

    First chapter is DONE.

--- hello_world.lean
+++ hello_world.lean
@@ -1,1 +1,11 @@
 #eval "Hello, Lean!"
+
+
+-- Exercieses
+
+#eval 42 + 19
+
+#eval String.append "A" (String.append "B" "C")
+#eval String.append (String.append "A" "B") "C"
+#eval if 3 == 3 then 5 else 7
+#eval if 3 == 4 then "equal" else "not equal"
