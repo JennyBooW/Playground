b4594d9219fdde429517a210dbcf6f0afc5e7e93
JennyBooW <verikgorohova@yandex.com>
2024-09-28 20:22:48 +0300

    Final for TODAY!

--- plymorthism.lean
+++ plymorthism.lean
@@ -50,7 +50,7 @@ def primes_under_ten : List Nat := [2, 3, 5, 7]
 def length (alpha : Type) (xs : List alpha) : Nat :=
   match xs with
   | []      => Nat.zero
-  | y :: ys => Nat.succ (length alpha ys)
+  | _ :: ys => Nat.succ (length alpha ys)
 
 #eval length Nat primes_under_ten
 
@@ -68,8 +68,7 @@ def words : List String := ["Hello", " ", "Lean", "!"]
 def length_inference {alpha : Type} (xs : List alpha) : Nat :=
   match xs with
   | []      => Nat.zero
-  | y :: ys => Nat.succ (length_inference ys)
-
+  | _ :: ys => Nat.succ (length_inference ys)
 
 
 #eval words.headD ""
@@ -107,27 +106,87 @@ def count_dogs (pets : List pet_name) : Nat :=
 -- Write a function to find the last entry in a list. It should return an Option.
 
 -- Test lists
-def numbers : List Nat    := [1, 2, 3, 4, 5]
-def letters : List String := ["A", "B", "C", "D"]
-def empty   : List Int    := []
+def numbers : List Nat      := [1, 2, 3, 4, 5]
+def letters : List String   := ["A", "B", "C", "D"]
+def empty   : List Int      := []
 
 def find_tail {α : Type} (xs : List α) : Option α :=
   match xs with
-  | List.nil => none
-  | List.cons x xs =>
-    match xs with
-    | List.nil  => some x
-    | List.cons _ ys => find_tail ys
+  | []      => none
+  | [x]     => some x
+  | _ :: ys => find_tail ys
 
 #eval find_tail numbers
+#eval find_tail letters
+#eval find_tail empty
+
+-- Write a function that finds the first entry in a list that satisfies a given
+-- predicate. Start the definition with def List.findFirst?
+-- {α : Type} (xs : List α) (predicate : α → Bool) : Option α :=
+
+def findFirst {α : Type} (xs : List α) (predicate : α → Bool) : Option α :=
+  match xs with
+  | []    => none
+  | y::ys => if predicate y
+    then y
+    else findFirst ys predicate
+
+-- Test predicate function
+def isEven (n : Nat) : Bool :=
+  match n with
+  | Nat.zero   => true
+  | Nat.succ k => not ( isEven k )
+
+#eval findFirst numbers isEven
+
+-- Write a function Prod.swap that swaps the two fields in a pair.
+-- Start the definition with def Prod.swap {α β : Type} (pair : α × β) : β × α :=
+
+def swap {α β : Type} (pair : α × β) : β × α :=
+  (pair.snd, pair.fst)
+
+#eval swap prd
+
+-- Rewrite the PetName example to use a custom datatype and compare it to the version
+-- that uses Sum.
+
+def custom_dog_cat {α β : Type} (x : α) (y : β) : α × β :=
+  (x, y)
 
 
--- Write a function that finds the first entry in a list that satisfies a given predicate. Start the definition with def List.findFirst? {α : Type} (xs : List α) (predicate : α → Bool) : Option α :=
--- Write a function Prod.swap that swaps the two fields in a pair. Start the definition with def Prod.swap {α β : Type} (pair : α × β) : β × α :=
--- Rewrite the PetName example to use a custom datatype and compare it to the version that uses Sum.
--- Write a function zip that combines two lists into a list of pairs. The resulting list should be as long as the shortest input list. Start the definition with def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) :=.
--- Write a polymorphic function take that returns the first n
---  entries in a list, where n
---  is a Nat. If the list contains fewer than n entries, then the resulting list should be the input list. #eval take 3 ["bolete", "oyster"] should yield ["bolete", "oyster"], and #eval take 1 ["bolete", "oyster"] should yield ["bolete"].
+def my_animals : List (String × String) :=
+  [ custom_dog_cat "Lammy" "Catty",  custom_dog_cat "Spot" "Dorothy"]
+
+#eval my_animals.head?
+
+
+-- Write a function zip that combines two lists into a list of pairs. The resulting list should be as long as the shortest input list. Start the definition with
+
+def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) :=
+  match xs, ys with
+  | [], _ => []
+  | _, [] => []
+  | x :: xs', y :: ys' => (x, y) :: zip xs' ys'
+
+def abc : List String := ["A", "B", "C"]
+def xyz : List String := ["X", "Y", "Z"]
+
+#eval zip abc xyz
+
+-- Write a polymorphic function "take" that returns the first n
+--  entries in a list, where n is a Nat. If the list contains fewer than n entries,
+-- then the resulting list should be the input list. #eval take 3 ["bolete", "oyster"]
+-- should yield ["bolete", "oyster"], and #eval take 1 ["bolete", "oyster"]
+-- should yield ["bolete"].
+
+def take {α : Type } (xs : List α) (n : Nat) : List α :=
+  match xs, n with
+  | [], _  => []
+  |  _, 0  => []
+  | _ :: xs', k' => take xs' (k' - 1)
+
+#eval take letters 1
+
 -- Using the analogy between types and arithmetic, write a function that distributes products over sums. In other words, it should have type α × (β ⊕ γ) → (α × β) ⊕ (α × γ).
+
 -- Using the analogy between types and arithmetic, write a function that turns multiplication by two into a sum. In other words, it should have type Bool × α → α ⊕ α.
